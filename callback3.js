c13480129335d2bd6aa55baec1562d72d64ea5b5
Sameer Srivastava <sameer.srivastava.17@mountblue.tech>
2021-09-14 16:13:53 +0530

    fixed problem 3

--- callback3.js
+++ callback3.js
@@ -4,20 +4,20 @@ const callback3 = (id, callback) => {
   if (typeof id == undefined || typeof callback == undefined) {
     console.log("invalid input");
   } else {
-    fs.readFile("../cards.json", "utf-8", (err, data) => {
-      if (err) {
-        callback(err);
-      } else {
-        let parsedData = JSON.parse(data);
-        let result;
-        if (id in parsedData) {
-          result = parsedData[id];
+    setTimeout(()=>{fs.readFile("../cards.json", "utf-8", (err, data) => {
+        if (err) {
+          callback(err);
         } else {
-          result = [];
+          let parsedData = JSON.parse(data);
+          let result;
+          if (id in parsedData) {
+            result = parsedData[id];
+          } else {
+            result = [];
+          }
+          callback(null, result);
         }
-        callback(null, result);
-      }
-    });
+      })},2000)
   }
 };
 module.exports = callback3;
